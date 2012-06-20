e82d39b3b911ba8803b611f1a79ac9fea14b928a
Javier Arturo Rodriguez <javier@rodriguez.org.mx>
2012-06-20 23:34:22 +0200

    Imported missing files.

---
+++ cpp_home/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('home.views',
+    # Examples:
+    # url(r'^$', 'cpp_home.views.home', name='home'),
+    # url(r'^cpp_home/', include('cpp_home.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'root'),
+    url(r'^about$', 'about'),
+    url(r'^region/$', 'regions'),
+    url(r'^country/$', 'countries'),
+    url(r'^country/(?P<country_id>\w+)$', 'country'),
+
+)
